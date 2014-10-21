0dae7c8e80d1bcbe3b7ebc68ab48cf40e02f0708
Mark Tauber <mmtauber@gmail.com>
2014-10-20 20:56:02 -0700

    remove replace prob.

---
+++ src/com/radar/interview/string/ReplaceRemove.java
@@ -0,0 +1,28 @@
+package com.radar.interview.string;
+
+import com.radar.interview.InterviewProblem;
+
+/**
+ * @author mtauber
+ *
+ * Write a function which takes as input a string 's', and removes each 'b'
+ * and replaces each 'a' with 'dd'. Use O(1) additional storage-assume 's' is stored in
+ * an array that has enough space for the final result.
+ *
+ */
+public class ReplaceRemove implements InterviewProblem {
+
+    public void replaceRemove(String input) {
+
+
+        System.out.println("Answer is: ");
+
+    }
+
+    @Override
+    public void runExample() {
+        replaceRemove("1233");
+        replaceRemove("-1002");
+        replaceRemove("100100100");
+    }
+}
